bf86d4e59406ac6b436e13f1df0515c3b3998160
张永国 <z13139332170@163.com>
2026-07-11 12:51:20 +0800

    Fix category page error: replace BACKEND_URL with getImageUrl in ProductCard

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { SearchQuery } from '~/generated/graphql';
+﻿import { SearchQuery } from '~/generated/graphql';
 import { Link } from '@remix-run/react';
 import { Price } from './Price';
 import { getImageUrl } from '~/constants';
@@ -12,15 +12,15 @@ export function ProductCard({
   currencyCode,
 }: ProductCardProps) {
   return (
-    <Link className="flex flex-col" prefetch="intent" to={`/products/${slug}`}>
+    <Link className='flex flex-col' prefetch='intent' to={/products/}>
       <img
-        className="rounded-xl flex-grow object-cover aspect-[7/8]"
-        alt=""
-        src={BACKEND_URL + (productAsset?.preview || '') + '?w=300&h=400'}
+      className='rounded-xl flex-grow object-cover aspect-[7/8]'
+      alt=''
+      src={getImageUrl(productAsset?.preview, { w: 300, h: 400 })}
       />
-      <div className="h-2" />
-      <div className="text-sm text-gray-700">{productName}</div>
-      <div className="text-sm font-medium text-gray-900">
+      <div className='h-2' />
+      <div className='text-sm text-gray-700'>{productName}</div>
+      <div className='text-sm font-medium text-gray-900'>
         <Price priceWithTax={priceWithTax} currencyCode={currencyCode} />
       </div>
     </Link>
