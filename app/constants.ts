d569d628bf89e5fcad79837fbac2561eee6f80cc
张永国 <z13139332170@163.com>
2026-07-13 20:22:28 +0800

    Fix product page image loading via proxy

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
-﻿export const APP_META_TITLE = 'Vendure Remix Storefront';
+export const APP_META_TITLE = 'Vendure Remix Storefront';
 export const APP_META_DESCRIPTION =
   'A headless commerce storefront starter kit built with Remix & Vendure';
 export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
@@ -11,15 +11,11 @@ export const BACKEND_URL = API_URL.replace('/shop-api', '');
 
 export function getImageUrl(preview: string | undefined, options: { w?: number; h?: number; preset?: string } = {}): string {
   if (!preview) {
-    return 'data:image/svg+xml,%3Csvg xmlns=\
-http://www.w3.org/2000/svg\ viewBox=\0
-0
-200
-200\%3E%3Crect fill=\%23f3f4f6\ width=\200\ height=\200\/%3E%3Ctext fill=\%239ca3af\ font-family=\sans-serif\ font-size=\14\ x=\50%25\ y=\50%25\ text-anchor=\middle\ dy=\.3em\%3ENo image%3C/text%3E%3C/svg%3E';
+    return 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"%3E%3Crect fill="%23f3f4f6" width="200" height="200"/%3E%3Ctext fill="%239ca3af" font-family="sans-serif" font-size="14" x="50%25" y="50%25" text-anchor="middle" dy=".3em"%3ENo image%3C/text%3E%3C/svg%3E';
   }
-
+  
   let url: string;
-
+  
   if (preview.startsWith('http')) {
     const assetPath = preview.replace(/^https?:\/\/[^\/]+\/assets\//, '/api/assets/');
     url = assetPath;
@@ -28,7 +24,7 @@ http://www.w3.org/2000/svg\ viewBox=\0
   } else {
     url = '/api/assets/' + preview;
   }
-
+  
   const params: string[] = [];
   if (options.w) params.push('w=' + options.w);
   if (options.h) params.push('h=' + options.h);
@@ -43,7 +39,7 @@ http://www.w3.org/2000/svg\ viewBox=\0
 /**
  * This function is used when running in Cloudflare Pages in order to set the API URL
  * based on an environment variable. Env vars work differently in CF Pages and are not available
- * on the process object (which does not exist). Instead, it needs to be accessed from the loader
+ * on the `process` object (which does not exist). Instead, it needs to be accessed from the loader
  * context, and if defined we use it here to set the API_URL var which will be used by the
  * GraphQL calls.
  *
