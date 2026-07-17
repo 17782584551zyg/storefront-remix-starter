7e7d36316ae271ec2fdebc7d962e634f4901aa56
张永国 <z13139332170@163.com>
2026-07-17 16:53:16 +0800

    Make product list span full width - lg:col-span-5

--- app/components/products/FilterableProductGrid.tsx
+++ app/components/products/FilterableProductGrid.tsx
@@ -47,7 +47,7 @@ export function FilterableProductGrid({
         setMobileFiltersOpen={setMobileFiltersOpen}
       />
       {result.items.length > 0 ? (
-        <div className="sm:col-span-5 lg:col-span-4 space-y-8 pb-16">
+        <div className="sm:col-span-5 lg:col-span-5 space-y-8 pb-16">
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
             {result.items.map((item) => (
               <ProductCard key={item.productId} {...item} />
