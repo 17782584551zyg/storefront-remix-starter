2c0c2d586c64ecfb600a5c6eabbc59d75a25b6f4
张永国 <z13139332170@163.com>
2026-07-24 11:45:07 +0800

    Remove HeroBanner carousel from homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,10 @@
-﻿import { HeroBanner } from '../components/home/HeroBanner';
-import { FeaturesSection } from '../components/home/FeaturesSection';
+﻿import { FeaturesSection } from '../components/home/FeaturesSection';
 import { ServicesSection } from '../components/home/ServicesSection';
 import { TestimonialsSection } from '../components/home/TestimonialsSection';
 
 export default function Index() {
   return (
     <div>
-      <HeroBanner />
       <FeaturesSection />
       <ServicesSection />
       <TestimonialsSection />
